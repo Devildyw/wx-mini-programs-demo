ed3a5a890de145092b924bf27f1518b279c919e9
Devildyw <1842501760@qq.com>
2024-04-24 23:25:14 +0800

    问答弹出层初弹错

--- demo_login/pages/selectCourse/QADetailInfo.js
+++ demo_login/pages/selectCourse/QADetailInfo.js
@@ -10,6 +10,15 @@ Page({
     style: 'height: 248rpx',
     activeValues: [0],
     ReplyList:[1,2,3,4,5,6,7,8],
+    show: false,
+    cur: {},
+    position: [
+      { value: 'top', text: '顶部弹出' },
+      { value: 'left', text: '左侧弹出' },
+      { value: 'center', text: '中间弹出' },
+      { value: 'bottom', text: '底部弹出' },
+      { value: 'right', text: '右侧弹出' },
+    ],
   },
 
   /**
@@ -21,6 +30,28 @@ Page({
     })
   },
 
+  handlePopup(e) {
+    const { item } = e.currentTarget.dataset;
+
+    this.setData(
+      {
+        cur: item,
+      },
+      () => {
+        this.setData({ visible: true });
+      },
+    );
+  },
+  onVisibleChange(e) {
+    this.setData({
+      visible: e.detail.visible,
+    });
+  },
+
+
+  onOpen(){
+    this.setData({ show : true});
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
